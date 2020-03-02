ff48c55c2040525111daf76bbbf2f6e9059498cd
Andreas Hofheinz <andreas.hofheinz@outlook.com>
2020-03-02 09:53:27 +0100

    Added accountWithLog and corresponing tests

--- bankr/R/account-r6.R
+++ bankr/R/account-r6.R
@@ -93,5 +93,134 @@ SafeAccount <- R6::R6Class("SafeAccount",
   )
 )
 
+TransactionLog <- R6::R6Class("TransactionLog",
+  private = list(
+    .trans_log = data.frame(
+      "Date" = as.Date(character(0)),
+      "Transaction" = character(0),
+      "Amount" = numeric(0),
+      "Balance" = numeric(0),
+      stringsAsFactors = FALSE
+    )
+  ),
+  active = list(
+    trans_log = function(value) {
+      if (missing(value)) {
+        private$.trans_log
+      } else {
+        stop("`$trans_log is read only`")
+      }
+    }
+  ),
+  public = list(
+    make_trans = function(trans_type, amount) {
+      checkmate::assert_choice(trans_type, c("deposit", "withdraw"))
+      checkmate::assert_integerish(amount, lower = 0, any.missing = FALSE, len = 1)
+      if (trans_type == "withdraw") {
+        amount <- -amount
+      }
+      if (nrow(private$.trans_log) == 0) {
+        balance <- amount
+      } else {
+        old_trans_log <- private$.trans_log
+        balance <- old_trans_log[nrow(old_trans_log), "Balance"] + amount
+      }
+      new_trans <- data.frame(date(), trans_type, amount, balance, stringsAsFactors = FALSE)
+      names(new_trans) <- c("Date", "Transaction", "Amount", "Balance")
+      new_translog <- rbind(private$.trans_log, new_trans)
+      private$.trans_log <- new_translog
+      cat("Transaction completed!")
+      invisible(self)
+    }
+  )
+)
 
+
+
+AccountWithLog <- R6::R6Class("AccountWithLog",
+  private = list(
+    #' @field balance Current account balance
+    .balance = NULL,
+    .trans_log = NULL
+  ),
+  active = list(
+    balance = function(value){
+      if (missing(value)) {
+        private$.balance
+      } else {
+        stop("`$balance is read only`")
+      }
+    },
+    trans_log = function(value){
+      if (missing(value)) {
+        private$.trans_log$trans_log
+      } else {
+        stop("`$trans_log is read only`")
+      }
+    }
+  ),
+  public = list(
+    #' @description
+    #' Create a new account object.
+    #' @return A new `Account` object with `balance` of 0.
+    initialize = function(balance = 0, trans_log = TransactionLog$new()) {
+      checkmate::assert_integerish(balance, lower = 0, any.missing = FALSE, len = 1)
+      checkmate::assert_class(trans_log, "R6")
+      private$.balance <- balance
+      private$.trans_log <- trans_log
+    },
+
+    #' @description
+    #' Withdraw money from bank account.
+    #' @param amount Amount to be withdrawn. Must be a whole number.
+    #' @examples
+    #' A <- Account$new()
+    #' A$deposit(100)
+    #' A$withdraw(50)
+    withdraw = function(amount) {
+      checkmate::assert_integerish(amount, lower = 0, any.missing = FALSE, len = 1)
+      private$.balance <- private$.balance - amount
+      private$.trans_log$make_trans("withdraw", amount)
+    },
+
+    #' @description
+    #' Deposit money into bank account.
+    #' @param amount Amount to be deposited. Must be a whole number.
+    #' @examples
+    #' A <- Account$new()
+    #' A$deposit(100)
+    deposit = function(amount) {
+      checkmate::assert_integerish(amount, lower = 0, any.missing = FALSE, len = 1)
+      private$.balance <- private$.balance + amount
+      private$.trans_log$make_trans("deposit", amount)
+    },
+    print_bank_statement = function(){
+      cat(paste0(rep("=", 60)), "\n   Bank Statement   \n", paste0(rep("=", 60)), "\n", sep = "")
+      private$.trans_log$trans_log
+    }
+  ),
+  cloneable = FALSE
+)
+
+# Is there a better solution?
+# https://stackoverflow.com/questions/53935619/changing-cloning-behavior-in-r6-classes
+AccountWithLog$set("public", "clone", function() {
+  cloned_acc <- AccountWithLog$new(private$.balance, private$.trans_log$clone())
+  cloned_acc
+})
+
+# acc_with_log <- AccountWithLog$new()
+# acc_with_log
+# acc_with_log$withdraw(1000)
+# acc_with_log$balance
+# acc_with_log$trans_log
+# acc_with_log$print_bank_statement()
+# acc_with_log$deposit(2500)
+# acc_with_log$balance
+# acc_with_log$trans_log
+# acc_with_log$print_bank_statement()
+# new_acc <- acc_with_log$clone()
+# new_acc$deposit(5000)
+# new_acc$print_bank_statement()
+# acc_with_log$print_bank_statement()
 
--- bankr/tests/testthat/test-account-r6.R
+++ bankr/tests/testthat/test-account-r6.R
@@ -48,3 +48,68 @@ test_that("Withdraw and deposit methods work correctly", {
   expect_error(my_safeaccount$withdraw <- -1000, "is not >= 0")
   expect_error(my_safeaccount$withdraw <- 10.5, "not close to an integer")
 })
+
+
+context("R6 TransactionLog")
+
+test_that("Transactions work correctly", {
+
+  my_translog <- TransactionLog$new()
+  tlog <- my_translog$trans_log
+  expect_equal(nrow(tlog), 0)
+  expect_true(checkmate::check_data_frame(tlog, types = c("date", "character", "integerish", "integerish")))
+
+  my_translog$make_trans("deposit", 1000)
+  tlog <- my_translog$trans_log
+  expect_true(tlog[1, "Transaction"] == "deposit", tlog[1, "Amount"] == 1000, tlog[1, "Balance"] == 1000)
+
+  my_translog$make_trans("withdraw", 500)
+  tlog <- my_translog$trans_log
+  expect_true(tlog[1, "Transaction"] == "deposit", tlog[1, "Amount"] == 1000, tlog[1, "Balance"] == 1000)
+  expect_true(tlog[2, "Transaction"] == "withdraw", tlog[1, "Amount"] == -500, tlog[1, "Balance"] == 500)
+
+  expect_error(my_translog$make_trans("no idea", 1000), "Must be element of set \\{'deposit','withdraw'}")
+  expect_error(my_translog$make_trans("deposit", "viel"), "Must be of type 'integerish'")
+})
+
+
+context("R6 AccountWithLog")
+
+test_that("Transactions work correctly", {
+
+  acc_with_log <- AccountWithLog$new()
+  expect_equal(acc_with_log$balance, 0)
+  tlog <- acc_with_log$trans_log
+  expect_equal(nrow(tlog), 0)
+  expect_true(checkmate::check_data_frame(tlog, types = c("date", "character", "integerish", "integerish")))
+
+  acc_with_log$withdraw(1000)
+  expect_equal(acc_with_log$balance, -1000)
+  tlog <- acc_with_log$trans_log
+  expect_true(tlog[1, "Transaction"] == "withdraw", tlog[1, "Amount"] == 1000, tlog[1, "Balance"] == 1000)
+
+  acc_with_log$deposit(2500)
+  expect_equal(acc_with_log$balance, 1500)
+  expect_equal(tlog[1, "Transaction"] == "deposit", tlog[1, "Amount"] == 2500, tlog[1, "Balance"] == 1500)
+
+  expect_error(acc_with_log$deposit(-1000), "is not >= 0")
+  expect_error(acc_with_log$deposit(10.5), "not close to an integer")
+  expect_error(acc_with_log$withdraw(-1000), "is not >= 0")
+  expect_error(acc_with_log$withdraw(10.5), "not close to an integer")
+})
+
+test_that("Cloning works correctly", {
+  acc_with_log <- AccountWithLog$new()
+  acc_with_log$deposit(2500)
+  tlog <- acc_with_log$trans_log
+  expect_true(tlog[1, "Transaction"] == "deposit", tlog[1, "Amount"] == 2500, tlog[1, "Balance"] == 2500)
+  new_acc <- acc_with_log$clone()
+  new_acc$deposit(5000)
+  tlog_clone <- new_acc$trans_log
+  expect_true(tlog_clone[2, "Transaction"] == "deposit", tlog_clone[2, "Amount"] == 5000,
+              tlog_clone[1, "Balance"] == 7500)
+  expect_true(nrow(acc_with_log$trans_log) == 1)
+})
+
+
+
